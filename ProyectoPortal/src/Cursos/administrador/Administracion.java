29301f79fa8c8ceec32252ce12300b111398a4ce
Crstndfp <thecris.danny@gmail.com>
2015-07-05 12:27:03 -0600

    se agrego todos lo metodos necesarios a la clase administracion para poder llamare a las clases necesarias

--- ProyectoPortal/src/Cursos/administrador/Administracion.java
+++ ProyectoPortal/src/Cursos/administrador/Administracion.java
@@ -18,16 +18,59 @@ public class Administracion {
         this.emf = emf;
     }
     
+    //////////// Modulo curso ///////////////////////////////////////
+    
     public void crearCurso(Object curso, Object ciclo, int year){
         CrearCursos cc = new CrearCursos(curso, ciclo, year);
         cc.instruccion(emf);
     }
+    
+    public void modificarCurso(int idSeccionCurso, int cupo){
+    
+    }
+    
+    
+    public void buscarCurso(){
+        
+    }
+    
+    public void eliminarCurso(){
+    
+    }
+    
+    ///////////////////////// Modulo SEccion //////////////////
+    
+    
     
     public void crearSeccion(Object seccion, int idcurso, String aula, int cupo){
         CrearSeccion cs = new CrearSeccion(seccion, aula, idcurso, cupo);
         cs.instruccion(emf);
     }
     
-    public void ModificarCupo(int idSeccionCurso, int cupo){
+    public void buscarSeccion(){
+    
+    }
+    
+    public void eliminarSeccion(){
+    }
+    
+    public void modificarSeccion(){
+    
+    }
+    
+    //////////////// modulo de ciclo //////////////////////////////
+    
+    public void crearCiclo(){
+    }
+    
+    public void buscarCiclo(){
+    
+    }
+    
+    public void eliminarCiclo(){
+    
+    }
+    public void modificarCiclo(){
+    
     }
 }
