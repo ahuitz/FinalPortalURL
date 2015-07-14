1bb48a316378157140b8d07e54c7a6e0d985892d
dvader33 <vhader33.dr@gmail.com>
2015-07-14 01:10:03 -0600

    interfaz para controlador

---
+++ ProyectoPortal/src/ModuloUsuarios/DualController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ModuloUsuarios;
+
+import Conexion.Config;
+import Controladores.PersonaJpaController;
+import Controladores.UsuarioJpaController;
+import Tablas.Persona;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+/**
+ *
+ * @author Vader33
+ */
+public class DualController implements Controlador{
+
+    private PersonaJpaController controladorpersona;
+    private UsuarioJpaController controladorusuario;
+    private EntityManager em;
+    private EntityManagerFactory emf;
+    private Config configuracion;
+    
+    @Override
+    public void Create(Persona persona) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
