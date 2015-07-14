696701f88dff6b6e6729d59dea3dcbbc1ee5f523
dvader33 <vhader33.dr@gmail.com>
2015-07-14 01:52:38 -0600

    Creando clases para creacion con strategy

---
+++ ProyectoPortal/src/ModuloUsuarios/EST.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ModuloUsuarios;
+
+/**
+ *
+ * @author Vader33
+ */
+public class EST implements TipoPersona{
+
+    @Override
+    public void crear() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
