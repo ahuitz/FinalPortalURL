6efacd55b5dd9efbd9e99ce3d10a4e517b90d1ec
ahuitz <ahuitz811@gmail.com>
2015-07-15 12:18:52 -0600

    metodo para busqueda de una persona de acuerdo al número de CARNE retorna una PERSONA

---
+++ ProyectoPortal/src/ModuloUsuarios/AccionesUsuario.java
@@ -0,0 +1,31 @@
+package ModuloUsuarios;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import Tablas.Persona;
+import Tablas.Tiposangre;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Rosario
+ */
+public class AccionesUsuario {
+    
+    public Persona buscarUsCarne(String carne,EntityManager em){        
+        Query q=em.createNamedQuery("Persona.findByCarne");
+        q.setParameter("carne", carne);
+        Persona persona=(Persona) q.getSingleResult();
+        return persona;
+        //System.out.println(persona.getApellido());
+    }
+}
